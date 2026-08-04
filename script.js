00b5acade0bf5eded615173e5d6c0c89a97a7347
HandharJunino <hjunino@gmail.com>
2026-08-04 21:16:56 +0100

    enhanced styling with js

---
+++ script.js
@@ -0,0 +1,13 @@
+const revealEls = document.querySelectorAll(".reveal");
+const io = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        io.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 0.15 },
+);
+revealEls.forEach((el) => io.observe(el));
